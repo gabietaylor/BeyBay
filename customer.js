514b9a3beafd112a9386ce1cf907658ee63ba286
gabietaylor <gabie.taylor@yahoo.com>
2017-06-23 17:59:45 -0400

    connected to database

--- customer.js
+++ customer.js
@@ -1,4 +1,12 @@
 var mysql = require("mysql");
-console.log(mysql);
+//console.log(mysql);
 var inquirer = require("inquirer");
-console.log(inquirer);
+//console.log(inquirer); 
+
+var connection = mysql.createConnection({
+	host	 : 'localhost',
+	user	 : 'root',
+	password : '',
+	database : 'beybay'
+});
+//console.log(connection);
