9cb41995f51e62ab8735ee0327a0622d8c17135e
gabietaylor <gabie.taylor@yahoo.com>
2017-06-25 14:00:28 -0400

    prompt is logging choices

--- app.js
+++ app.js
@@ -14,5 +14,38 @@ var connection = mysql.createConnection({
 connection.connect(function(err) {
     if (err) throw err;
     //console.log("MySQL server connected with the ID" + connection.threadId)
-    // mainMenu()
+    mainMenu()
 });
+
+function mainMenu() {
+    inquirer.prompt([{
+        name: "list",
+        type: "rawlist",
+        message: "Welcome to BeyBay: the One Stop Shop for all you slaying needs!! Are you a:",
+        choices: ["Customer", "Manager", "Supervisor"]
+    }]).then(function(input) {
+        switch (input.list) {
+            case "Customer":
+                customer();
+                break;
+            case "Manager":
+                manager();
+                break;
+            case "Supervisor":
+                supervisor();
+                break;
+        }
+    });
+}
+
+/*function customer() {
+
+}
+
+function manager() {
+
+}
+
+function supervisor() {
+
+}*/
