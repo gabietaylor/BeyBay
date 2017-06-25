3f96d89550595038054e6dc51800e6529a234ef7
gabietaylor <gabie.taylor@yahoo.com>
2017-06-25 01:20:57 -0400

    menu uptions to grab funtions work funtion layout made

--- manager.js
+++ manager.js
@@ -14,4 +14,45 @@ var connection = mysql.createConnection({
 connection.connect(function(err) {
     if (err) throw err;
     //console.log("MySQL server connected with the ID" + connection.threadId)
+    menu()
 });
+
+function menu() {
+	inquirer.prompt([{
+		name: "list",
+		type: "rawlist",
+		message: "Welcome Manager: Choose from the menu:",
+		choices: ["View Products for Sale", "View Low Inventory", "Add to Inventory", "Add New Product"]
+	}]).then(function (input) {
+		switch(input.list) {
+			case "View Products for Sale":
+                saleProducts();
+                break;
+            case "View Low Inventory":
+                lowInventory();
+                break;
+            case "Add to Inventory":
+                addInventory();
+                break;
+            case "Add New Product":
+                addProduct();
+                break;
+		}
+	});
+}
+
+funtion saleProducts() {
+
+}
+
+funtion lowInventory() {
+	
+}
+
+funtion addInventory() {
+	
+}
+
+funtion addProduct() {
+	
+}
