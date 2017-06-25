03a08308f75a4ad33e155bc205397ab712b73abd
gabietaylor <gabie.taylor@yahoo.com>
2017-06-25 02:33:44 -0400

    initial menu created

--- supervisor.js
+++ supervisor.js
@@ -10,3 +10,41 @@ var connection = mysql.createConnection({
     database: 'beybay'
 });
 //console.log(connection);
+
+connection.connect(function(err) {
+    if (err) throw err;
+    //console.log("MySQL server connected with the ID" + connection.threadId)
+    menu()
+});
+
+function menu() {
+    inquirer.prompt([{
+        name: "menu",
+        type: "rawlist",
+        message: "Please choose from one of the following options",
+        choices: ["View Product Sales by Department", "Create New Department"]
+    }]).then(function(input) {
+        switch (input.menu) {
+            case "View Products Sales by Department":
+                salesDept();
+                break;
+            case "Create New Department":
+                newDept();
+                break;
+            default:
+                console.log("Please choose what you want to do!")
+        }
+
+    });
+
+}
+
+// Sales Dept
+function salesDept() {
+
+}
+
+// New Dept
+function newDept() {
+
+}
